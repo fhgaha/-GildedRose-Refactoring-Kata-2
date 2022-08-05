b905bfe6254fca3e03454987067c98bd56a88d93
fhgaha <antsy1992@gmail.com>
2022-08-05 20:29:21 +0300

    r UpdateDefault

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -37,23 +37,26 @@ namespace csharp
             {
                 return;
             }
-            else
+         
+            UpdateDefault(item);
+            return;
+        }
+
+        private static void UpdateDefault(Item item)
+        {
+            if (item.Quality > 0)
+            {
+                item.Quality = item.Quality - 1;
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
             {
                 if (item.Quality > 0)
                 {
                     item.Quality = item.Quality - 1;
                 }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Quality > 0)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-                return;
             }
         }
 
