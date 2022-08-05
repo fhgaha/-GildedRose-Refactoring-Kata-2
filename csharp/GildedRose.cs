4ea4deee48349d151120a93ed406e9bf4aa834b0
fhgaha <antsy1992@gmail.com>
2022-08-05 20:13:20 +0300

    r UpdateBackstagePasses

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -29,33 +29,7 @@ namespace csharp
 
             if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-
-                    if (item.SellIn < 11)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-
-                    if (item.SellIn < 6)
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0)
-                {
-                    item.Quality = item.Quality - item.Quality;
-                }
+                UpdateBackstagePasses(item);
                 return;
             }
             else
@@ -84,6 +58,37 @@ namespace csharp
                     }
                 }
                 return;
+            }
+        }
+
+        private static void UpdateBackstagePasses(Item item)
+        {
+            if (item.Quality < 50)
+            {
+                item.Quality = item.Quality + 1;
+
+                if (item.SellIn < 11)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+
+                if (item.SellIn < 6)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = item.Quality - item.Quality;
             }
         }
 
